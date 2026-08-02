759bacd8305e121fb5156602b9e2e6464c846061
Simon Berger <simon@siku2.io>
2026-08-02 05:08:26 +0200

    fix: use extraConfig instead of structuredExtraConfig for kernel options

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -288,34 +288,34 @@ in
         {
           name = "native-rpi-builtins";
           patch = null;
-          structuredExtraConfig = with lib.kernel; {
-            BLK_DEV_DM = yes;
-            DM_VERITY = yes;
-            SQUASHFS = yes;
-            SQUASHFS_ZSTD = yes;
-            EXT4_FS = yes;
-            OVERLAY_FS = yes;
-            MMC = yes;
-            MMC_BLOCK = yes;
-            MMC_SDHCI = yes;
-            MMC_SDHCI_IPROC = yes;
-            MMC_SDHCI_OF_ARASAN = yes;
-            CRYPTO_SHA256 = yes;
-            ZSTD_COMPRESS = yes;
-            ZSTD_DECOMPRESS = yes;
-          };
+          extraConfig = ''
+            BLK_DEV_DM y
+            DM_VERITY y
+            SQUASHFS y
+            SQUASHFS_ZSTD y
+            EXT4_FS y
+            OVERLAY_FS y
+            MMC y
+            MMC_BLOCK y
+            MMC_SDHCI y
+            MMC_SDHCI_IPROC y
+            MMC_SDHCI_OF_ARASAN y
+            CRYPTO_SHA256 y
+            ZSTD_COMPRESS y
+            ZSTD_DECOMPRESS y
+          '';
         }
         {
           name = "trim-kernel-size";
           patch = null;
-          structuredExtraConfig = with lib.kernel; {
-            DEBUG_INFO_BTF = lib.mkForce no;
-            KALLSYMS_ALL = lib.mkForce no;
-            HAMRADIO = lib.mkForce no;
-            SOUND = lib.mkForce no;
-            WIRELESS = lib.mkForce no;
-            STAGING = lib.mkForce no;
-          };
+          extraConfig = ''
+            DEBUG_INFO_BTF n
+            KALLSYMS_ALL n
+            HAMRADIO n
+            SOUND n
+            WIRELESS n
+            STAGING n
+          '';
         }
       ];
     };
