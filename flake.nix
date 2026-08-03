fad1da62a5a8b63aa1b513e6be52439ec397da47
Simon Berger <simon@siku2.io>
2026-08-04 01:13:45 +0200

    fix: mkForce rp_filter in dev image to resolve conflict

--- flake.nix
+++ flake.nix
@@ -252,8 +252,8 @@
               users.users.root.hashedPassword = lib.mkForce "";
               networking.firewall.enable = lib.mkForce false;
               boot.kernel.sysctl = {
-                "net.ipv4.conf.all.rp_filter" = 0;
-                "net.ipv4.conf.default.rp_filter" = 0;
+                "net.ipv4.conf.all.rp_filter" = lib.mkForce 0;
+                "net.ipv4.conf.default.rp_filter" = lib.mkForce 0;
               };
             })
           ];
