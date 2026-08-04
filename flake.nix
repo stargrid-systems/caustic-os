1a72e177aa7e2b81a5a55f4819e0eabc740fd913
Simon Berger <simon@siku2.io>
2026-08-04 16:56:47 +0200

    fix: lambda syntax in list

--- flake.nix
+++ flake.nix
@@ -221,14 +221,14 @@
           imageId = "caustic-os-dev";
           otaRegistry = "ghcr.io/stargrid-systems/caustic-os-dev";
           extraModules = [
-            { lib, ... }: {
+            ({ lib, ... }: {
               users.users.root.hashedPassword = "";
               networking.firewall.enable = false;
               boot.kernel.sysctl = {
                 "net.ipv4.conf.all.rp_filter" = 0;
                 "net.ipv4.conf.default.rp_filter" = 0;
               };
-            }
+            })
           ];
         };
       };
